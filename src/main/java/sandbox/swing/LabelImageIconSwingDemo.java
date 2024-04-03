2dff35fcb2e537cef330699911b8249666eb49e6
Kirill Vinogradov <vikian050194@gmail.com>
2024-04-03 23:01:36 +0200

    add ImageIcon sample

---
+++ src/main/java/sandbox/swing/LabelImageIconSwingDemo.java
@@ -0,0 +1,34 @@
+package sandbox.swing;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.SwingUtilities;
+
+public class LabelImageIconSwingDemo {
+
+    public LabelImageIconSwingDemo() {
+        String title = "A Label With ImageIcon Demo Application";
+
+        JFrame frame = new JFrame(title);
+
+        frame.setSize(300, 100);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        frame.setVisible(true);
+
+        var fc = new JFileChooser();
+        fc.showOpenDialog(frame);
+        var imageIcon = new ImageIcon(fc.getSelectedFile().getAbsolutePath());
+
+        JLabel label = new JLabel(fc.getSelectedFile().getName(), imageIcon, JLabel.CENTER);
+
+        frame.add(label);
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(LabelImageIconSwingDemo::new);
+    }
+}
