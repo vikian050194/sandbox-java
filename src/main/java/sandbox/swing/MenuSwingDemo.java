27eccff9f22dc94de49175e263d1945af34623a0
Kirill Vinogradov <vikian050194@gmail.com>
2024-04-22 22:38:13 +0200

    add mnemonics and accelerators

--- src/main/java/sandbox/swing/MenuSwingDemo.java
+++ src/main/java/sandbox/swing/MenuSwingDemo.java
@@ -3,11 +3,14 @@ package sandbox.swing;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.InputEvent;
+import java.awt.event.KeyEvent;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.KeyStroke;
 import javax.swing.SwingUtilities;
 
 public class MenuSwingDemo implements ActionListener {
@@ -29,10 +32,19 @@ public class MenuSwingDemo implements ActionListener {
         var bar = new JMenuBar();
 
         var file = new JMenu("File");
-        file.add(new JMenuItem("Open")).addActionListener(this);
-        file.add(new JMenuItem("Close")).addActionListener(this);
-        file.add(new JMenuItem("Save")).addActionListener(this);
-        file.add(new JMenuItem("Exit")).addActionListener(this);
+        file.setMnemonic(KeyEvent.VK_F);
+        var open = file.add(new JMenuItem("Open", KeyEvent.VK_O));
+        open.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_O, InputEvent.CTRL_DOWN_MASK));
+        open.addActionListener(this);
+        var close = file.add(new JMenuItem("Close", KeyEvent.VK_C));
+        close.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_C, InputEvent.CTRL_DOWN_MASK));
+        close.addActionListener(this);
+        var save = file.add(new JMenuItem("Save", KeyEvent.VK_S));
+        save.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S, InputEvent.CTRL_DOWN_MASK));
+        save.addActionListener(this);
+        var exit = file.add(new JMenuItem("Exit", KeyEvent.VK_E));
+        exit.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_E, InputEvent.CTRL_DOWN_MASK));
+        exit.addActionListener(this);
         bar.add(file);
 
         var options = new JMenu("Options");
